8cbf2aa89ffb372b1e6eb5f693992350e7669066
biyuqi <biyuqiwan@163.com>
2019-10-13 15:30:32 +0800

    feat: not found page change

--- src/components/NotExist.vue
+++ src/components/NotExist.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="not-found-page">
     Page can not found. please back to: <br />
-    <a href="/">Home Page</a>.
+    <button class="not-found-page__button" @click="goHome">Home Page</button>.
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    goHome () {
+      this.$router.push({
+        path: '/tech-blog'
+      })
+    }
+  }
+}
+</script>
 <style lang="scss">
 .not-found-page {
   text-align: center;
   font-size: 16px;
   height: 100px;
   line-height: 100px;
+  font-size: 18px;
+
+  &__button {
+    padding: 10px 15px;
+    background-color: #00beb3;
+    color: #fff;
+  }
 }
 </style>
