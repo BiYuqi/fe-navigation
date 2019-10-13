9827798a63f7eeac5d7286c38e67baed8f5f9b01
biyuqi <biyuqiwan@163.com>
2019-10-13 16:47:50 +0800

    feat: deal with the parse json error

--- generate-data.js
+++ generate-data.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const chalk = require('chalk')
 const glob = require('glob')
 
 const jsonPath = path.resolve(__dirname, './jsondata/data/*.json')
@@ -7,8 +8,15 @@ const generateData = {}
 const files = glob.sync(jsonPath)
 
 files.forEach(file => {
-  const content = JSON.parse(fs.readFileSync(file, 'utf-8'))
-  Object.assign(generateData, content)
+  try {
+    const content = JSON.parse(fs.readFileSync(file, 'utf-8'))
+    Object.assign(generateData, content)
+  } catch (error) {
+    console.log(`
+    ${chalk.green('Parse json file got error')}:
+    ${chalk.red(error.message)}
+    `)
+  }
 })
 
 fs.writeFileSync('./jsondata/basedata.json', JSON.stringify(generateData))
