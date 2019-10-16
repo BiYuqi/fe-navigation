73b08f23357e1b681e924840ca161b57ab229fbc
BiYuqi <biyuqiwan@163.com>
2019-10-16 22:50:36 +0800

    chore: extract the script file

--- scripts/formatJson.js
+++ scripts/formatJson.js
@@ -4,7 +4,8 @@ const chalk = require('chalk')
 const glob = require('glob')
 const jsonFormat = require('json-format')
 
-const jsonPath = path.resolve(__dirname, './jsondata/data/*.json')
+const resolve = dir => path.resolve(__dirname, dir)
+const jsonPath = resolve('../jsondata/data/*.json')
 const files = glob.sync(jsonPath)
 
 const config = {
@@ -16,14 +17,14 @@ files.forEach(file => {
   try {
     const content = JSON.parse(fs.readFileSync(file, 'utf-8'))
     const fileName = file.match(/([^/]+)\.json$/)[1]
-    fs.writeFile(`./jsondata/data/${fileName}.json`, jsonFormat(content, config), error => {
+    fs.writeFile(resolve(`../jsondata/data/${fileName}.json`), jsonFormat(content, config), error => {
       if (error) {
         console.log(`
-        ${chalk.green('formatted json file got error')}:
+        ${chalk.green('Formatted json file got error')}:
         ${chalk.red(error.message)}
         `)
       }
-      console.log(chalk.cyanBright(`formatted json file success: ./jsondata/data/${fileName}.json`))
+      console.log(chalk.cyanBright(`Formatted json file success: /jsondata/data/${fileName}.json`))
     })
   } catch (error) {
     console.log(`
