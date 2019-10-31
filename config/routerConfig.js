fe8723ff44feb2835b10e3f34fe09f853dbdfd0c
BiYuqi <biyuqiwan@163.com>
2019-10-31 17:50:36 +0800

    feat: Reclassify the content title

--- config/routerConfig.js
+++ config/routerConfig.js
@@ -1,5 +1,6 @@
 export const routerPage = [
   ['tech-blog', '技术博客'],
+  ['tech-team', '社区团队'],
   ['framework', '框架类库'],
   ['mini-program', '小程序'],
   ['javascript', 'Javascript'],
@@ -8,7 +9,8 @@ export const routerPage = [
   ['build-tool', '构建工具'],
   ['tools-menu', '工具手册'],
   ['tech-topic', '技术专题'],
-  ['visualization', '可视化方案'],
+  ['visualization', '图形动效'],
+  ['online-tools', '在线工具'],
   ['awesome-library', 'NPM开源库'],
   ['git', '版本控制'],
   ['interview', '面试宝典'],
